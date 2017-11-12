a681586cc5b8085b3e0d60723211497619b04985
Mathias Colpaert <colpaert.mathias@gmail.com>
2017-11-12 10:56:20 +0100

    update script

--- start-geth.sh
+++ start-geth.sh
@@ -1,13 +1,12 @@
 echo "Geth Starting..."
 
 RPC_ADDR="$(hostname -i)"
-RPC_CORS_DOMAIN="*"
 
 geth_cmd="geth --fast \
   --rpc \
   --rpcaddr ${RPC_ADDR} \
   --rpcport 8545 \
-  --rpccorsdomain ${RPC_CORS_DOMAIN}"
+  --rpccorsdomain \*"
 
 echo $geth_cmd
 exec $geth_cmd 
